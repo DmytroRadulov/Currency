3c24867aa25b4a39eab8279bee5f6c153b004402
DmytroRadulov <radulovdima1995@gmail.com>
2022-09-28 13:55:08 +0300

    lessons_7

--- blog/public/index.php
+++ blog/public/index.php
@@ -7,18 +7,6 @@ require_once __DIR__ . '/../src/Models/Category.php';
 require_once __DIR__ . '/../src/Models/Post.php';
 require_once __DIR__ . '/../src/Models/Tag.php';
 
-
-//$categories = blog\src\Models\Category::all();
-//$tags = blog\src\Models\Tag::all();
-
 /** @var $blade */
 
 echo $blade->make('pages/index')->render();
-//echo $blade->make('pages/category',[
-//    'categories'=>$categories,
-//])->render();
-//
-//
-//echo $blade->make('pages/tag',[
-//    'tags'=>$tags
-//])->render();
