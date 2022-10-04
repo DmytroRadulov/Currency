c7b1ca8fdbbd07a7ad3a509d92d7485db8a3be91
DmytroRadulov <radulovdima1995@gmail.com>
2022-10-04 11:12:16 +0300

    lessons_6

--- Dz6/index.php
+++ Dz6/index.php
@@ -10,44 +10,47 @@ use Src\Models\Tag;
 use Src\Models\Post;
 use Src\Models\Category;
 
-// Category
-
+//Category
+//
+//$model = new Category();
 //$model->title = 'football';
 //$model->slug = 'Real';
 //$model->save();
-
+//
+//$model = new Category();
 //$model->title = 'football2';
 //$model->slug = 'Barcelona';
 //$model->save();
-
+//
+//$model = new Category();
 //$model->title = 'football3';
 //$model->slug = 'Sevilla';
 //$model->save();
-
 //
+//$model = new Category();
 //$model->title = 'football4';
 //$model->slug = 'Atletico';
 //$model->save();
-
-
+//
+//$model = new Category();
 //$model->title = 'football5';
 //$model->slug = 'Valencia';
 //$model->save();
-
-
+//
+//$model = new Category();
 //$model->title = 'football6';
 //$model->slug = 'Mallorca';
 //$model->save();
-
-
+//
+//$model = new Category();
 //$model->title = 'football3';
 //$model->slug = 'Sevilla';
 //$model->save();
 
-//$category = Category::find(8);
-//$category->title = 'APL';
-//$category->slug = 'Arsenal';
-//$category->save();
+//$category2 = Category::find(8);
+//$category2->title = 'APL';
+//$category2->slug = 'Arsenal';
+//$category2->save();
 
 //$categories = Category::find(11);
 //$categories->delete();
@@ -62,77 +65,77 @@ use Src\Models\Category;
 
 // Post
 
-//$category = Category::find(7);
+//$category2 = Category::find(7);
 //$post = new Post();
 //$post->title = 'ooo';
 //$post->slug = 'ooo';
 //$post->body = 'ooo';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
-//$category = Category::find(6);
+//$category2 = Category::find(6);
 //$post = new Post();
 //$post->title = 'xxx';
 //$post->slug = 'xxx';
 //$post->body = 'xxx';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 //
-//$category = Category::find(9);
+//$category2 = Category::find(9);
 //$post = new Post();
 //$post->title = 'eee';
 //$post->slug = 'eee';
 //$post->body = 'eee';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
-//$category = Category::find(8);
+//$category2 = Category::find(8);
 //$post = new Post();
 //$post->title = 'aaa';
 //$post->slug = 'aaa';
 //$post->body = 'aaa';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
-//$category = Category::find(7);
+//$category2 = Category::find(7);
 //$post = new Post();
 //$post->title = 'qqqq';
 //$post->slug = 'qqqq';
 //$post->body = 'qqqq';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
-//$category = Category::find(10);
+//$category2 = Category::find(10);
 //$post = new Post();
 //$post->title = 'asdcxv';
 //$post->slug = 'xcvxcvxc';
 //$post->body = 'xcvxcv';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
-//$category = Category::find(9);
+//$category2 = Category::find(9);
 //$post = new Post();
 //$post->title = 'asdas';
 //$post->slug = 'dasas';
 //$post->body = 'asdasd';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
-//$category = Category::find(10);
+//$category2 = Category::find(10);
 //$post = new Post();
 //$post->title = 'tesrt';
 //$post->slug = 'tesrt';
 //$post->body = 'tesrt';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
 
-//$category = Category::find(10);
+//$category2 = Category::find(10);
 //$post = new Post();
 //$post->title = 'asdasd';
 //$post->slug = 'tasdasd';
 //$post->body = 'asdasd';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
 //$posts = Post::find(1);
@@ -165,92 +168,67 @@ use Src\Models\Category;
 //$post = Post::find(10);
 //$post->tags()->sync([10,3,1]);
 
-//$category = Category::find(9);
+//$category2 = Category::find(9);
 //$post = Post::find(10);
 //$post->title = '11111';
 //$post->slug = '11111';
 //$post->body = '1111';
-//$post->category_id = $category->id;
+//$post->category_id = $category2->id;
 //$post->save();
 
 
 // Tag
 
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'rrr';
 //$model2->slug = 'rrr';
 //$model2->save();
 
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'ff';
 //$model2->slug = 'ff';
 //$model2->save();
 
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'gg';
 //$model2->slug = 'gg';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'mm';
 //$model2->slug = 'mm';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'hh';
 //$model2->slug = 'hh';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'as';
 //$model2->slug = 'as';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'ds';
 //$model2->slug = 'ds';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'fd';
 //$model2->slug = 'vc';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'bv';
 //$model2->slug = 'bv';
 //$model2->save();
 //
-//$model2 = new Tag;
+//$model2 = new Tag();
 //$model2->title = 'vb';
 //$model2->slug = 'vb';
 //$model2->save();
 //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //use Src\Models\Category;
